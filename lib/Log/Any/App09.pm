02d2416f32881e17fc21e72566313f494bf2fec8
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-12-27 10:01:38 +0700

    Add check to make sure that we load the correct Log::Any::App

--- lib/Log/Any/App09.pm
+++ lib/Log/Any/App09.pm
@@ -1,5 +1,10 @@
 package Log::Any::App09;
 # VERSION
-use Log::Any::App;
+require Log::Any::App;
+die "We don't have the correct version of Log::Any::App ".
+    "($Log::Any::App09::VERSION vs $Log::Any::App::VERSION), ".
+    "please reinstall Log-Any-App09\n"
+    unless $Log::Any::App::VERSION eq $Log::Any::App09::VERSION;
+
 1;
 # ABSTRACT: An easy way to use Log::Any in applications
